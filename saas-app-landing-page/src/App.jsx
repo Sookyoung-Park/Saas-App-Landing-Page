743fd402048409e5f9c735ea710ce40bd2101f1b
Soo <parksk971031@gmail.com>
2024-10-18 15:45:12 -0400

    hero section completed

--- saas-app-landing-page/src/App.jsx
+++ saas-app-landing-page/src/App.jsx
@@ -1,10 +1,21 @@
 import Header from "./sections/header"
+import Hero from "./sections/Hero"
+import Features from "./sections/Features"
+import Pricing from "./sections/Pricing"
+import Faq from './sections/Faq'
+import Testimonials from './sections/Testimonials'
 
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero/>
+      {/* <Features /> */}
+      {/* <Pricing />
+      <Faq />
+      <Testimonials />  */}
+
     </main>
   )
 }

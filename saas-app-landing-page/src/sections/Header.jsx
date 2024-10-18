bf87d8b6fe7806e20aa4a8902d99935f6f426b6d
Soo <parksk971031@gmail.com>
2024-10-18 14:23:39 -0400

    ongoing mobile menu bar

--- saas-app-landing-page/src/sections/Header.jsx
+++ saas-app-landing-page/src/sections/Header.jsx
@@ -1,4 +1,5 @@
 import {Link as LinkScroll} from 'react-scroll'
+import {useState} from 'react'
 
 const NavLink=({title})=>(
     <LinkScroll
@@ -9,39 +10,56 @@ const NavLink=({title})=>(
 )
 
 const Header = () => {
-  return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
-        <div className="container flex h-14 items-center max-lg:px-5">
-            <a className="lg:hidden flex-1 cursor-pointer z-2">
-                <img src="/images/xora.svg" width={115} height={55} alt="logo"/>
-            </a>
-            <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
-                <nav className='max-lg:relative max-lg:z-2 max-lg:my-auto'>
-                    {/* from styling items tmr 26:34 need to fix laptop size from making div*/}
-                    <ul className="flex max-lg:block max-lg:px-12">
-                        <li className="nav-li">
-                            <NavLink title='Features' />
-                            <div className="dot"/>
-                            <NavLink title='Pricing' />
-                        </li>
+    // track hanburger button is clicked
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <header className="fixed top-0 left-0 z-50 w-full py-10">
+            <div className="container flex h-14 items-center max-lg:px-5">
+                <a className="lg:hidden flex-1 cursor-pointer z-2">
+                    <img src="/images/xora.svg" width={115} height={55} alt="logo"/>
+                </a>
 
-                        <li className="nav-logo">
-                            <LinkScroll>
-                                <img src="/images/xora.svg" width={160} height={55} alt="logo"/>
-                            </LinkScroll>
-                        </li>
+                <div className='w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0'>
+                    <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+                        <nav className='max-lg:relative max-lg:z-2 max-lg:my-auto'>
+                            {/* from styling items tmr 26:34 need to fix laptop size from making div*/}
+                            <ul className="flex max-lg:block max-lg:px-12">
+                                <li className="nav-li">
+                                    <NavLink title='Features' />
+                                    <div className="dot"/>
+                                    <NavLink title='Pricing' />
+                                </li>
 
-                        <li className="nav-li">
-                            <NavLink title='FAQ' />
-                            <div className="dot"/>
-                            <NavLink title='Dowload' />
-                        </li>
-                    </ul>
-                </nav>
+                                <li className="nav-logo">
+                                    <LinkScroll>
+                                        <img src="/images/xora.svg" width={160} height={55} alt="logo"/>
+                                    </LinkScroll>
+                                </li>
+
+                                <li className="nav-li">
+                                    <NavLink title='FAQ' />
+                                    <div className="dot"/>
+                                    <NavLink title='Dowload' />
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <button 
+                className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+                onClick={() => setIsOpen((prevState)=> !prevState)}
+                >
+                    <img 
+                    src= {`/images/${isOpen ? 'close' : 'magic'}.svg`}
+                    alt="magic"
+                    className='size-1/2 object-contain'
+                    />
+                </button>
+
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header

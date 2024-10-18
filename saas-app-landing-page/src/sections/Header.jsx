e649332630496fe034e94a1d21a361e9cb95ac58
Soo <parksk971031@gmail.com>
2024-10-18 00:20:28 -0400

    ongoing

--- saas-app-landing-page/src/sections/Header.jsx
+++ saas-app-landing-page/src/sections/Header.jsx
@@ -1,7 +1,9 @@
 import {Link as LinkScroll} from 'react-scroll'
 
 const NavLink=({title})=>(
-    <LinkScroll>
+    <LinkScroll
+    className='base-bold text-p4 uppercase transition-colors duration-1000 cursor-pointer hover:text-p1 max-lg:h5'
+    >
         {title}
     </LinkScroll>
 )
@@ -13,9 +15,9 @@ const Header = () => {
             <a className="lg:hidden flex-1 cursor-pointer z-2">
                 <img src="/images/xora.svg" width={115} height={55} alt="logo"/>
             </a>
-            <div className="w-full">
-                <nav>
-                    {/* from styling items tmr 23:19*/}
+            <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+                <nav className='max-lg:relative max-lg:z-2 max-lg:my-auto'>
+                    {/* from styling items tmr 26:34 need to fix laptop size from making div*/}
                     <ul className="flex max-lg:block max-lg:px-12">
                         <li className="nav-li">
                             <NavLink title='Features' />

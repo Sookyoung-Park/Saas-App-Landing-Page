d8b7cfa47facec09671a9b1adbb8fb626932224f
Soo <parksk971031@gmail.com>
2024-10-20 14:36:14 -0400

    complete faq

--- saas-app-landing-page/src/sections/Faq.jsx
+++ saas-app-landing-page/src/sections/Faq.jsx
@@ -29,14 +29,22 @@ const Faq = () => {
                   className='size-1/2'/>
                 </div>
 
-                <div>
+                <div className='relative flex-1 pt-24'>
                 {faq.slice(0, halfLength).map((item, index) => (
                   <div>
                     <FaqItem key={item.id} item={item} index={index}/>
                   </div>
                 ))}
                 </div>
+                <div className='relative flex-1 lg:pt-24'>
+                {faq.slice(halfLength).map((item, index) => (
+                  <div>
+                    <FaqItem key={item.id} item={item} index={halfLength+index}/>
+                  </div>
+                ))}
+                </div>
             </div>
+            <div className='faq-lin_after aboslute left-[calc-(50%-1px)] -top-1 -z-1 h-full w-0.5 bg-s2 max-lg:hidden'/>
           </div>
 
         </div>

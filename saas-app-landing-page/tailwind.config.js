49294867e7bfcc3daa93fccb7efd038961a9895c
Soo <parksk971031@gmail.com>
2024-10-21 13:54:48 -0400

    complete redesign

--- saas-app-landing-page/tailwind.config.js
+++ saas-app-landing-page/tailwind.config.js
@@ -31,7 +31,7 @@ export default {
         200: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 16px 24px rgba(0, 0, 0, 0.25), inset 0px 4px 10px #3391FF',
         300: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 16px 24px rgba(0, 0, 0, 0.25), inset 0px 3px 6px #1959AD',
         400: 'inset 0px 2px 4px 0 rgba(255, 255, 255, 0.05)',
-        500: '0px 16px 24px rgba(0, 0, 0, 0.14), 0px -14px 48px rgba(40, 51, 111, 0.3)',
+        500: '0px 8px 24px rgba(0, 0, 0, 0.14), 0px -14px 48px rgba(40, 51, 111, 0.07)',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
